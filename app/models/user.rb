94ac1afb5d585eb7a97af09dedfd8f7e5d66430d
AdiChat <aditianhacker@gmail.com>
2015-12-08 16:27:24 +0530

    app folder added

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  has_many :articles, dependent: :destroy
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+end
