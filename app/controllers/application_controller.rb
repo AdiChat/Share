cece640750018709a6f1828903111c1510907d29
Aditya Chatterjee <aditianhacker@gmail.com>
2016-08-14 01:35:01 +0530

    Cleaning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   
   def require_user
     if !logged_in?
-      flash[:danger] = "You must be logged in to perform that action"
+      flash[:danger] = "You must be logged in to perform that action."
       redirect_to root_path
     end
   end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
   
     def require_same_user
       if current_user != @article.user and !current_user.admin?
-        flash[:danger] = "You can only edit or delete your own articles"
+        flash[:danger] = "You can only edit or delete your own articles. Contact Admin for upgrading your profile."
         redirect_to root_path
       end
     end
